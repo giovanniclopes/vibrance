83018329c03a56c9444d497095f6c091a2044548
Giovanni Lopes <92595678+giovanniclopes@users.noreply.github.com>
2022-11-09 08:14:28 -0300

    header component created

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,30 @@
 import Logo from '../public/logo.png';
+import cartIcon from '../assets/cart.png';
+import userIcon from '../assets/login.png';
+import brazilFlag from '../assets/brazilflag.png';
 
 export default function Header() {
     return (
         <>
             <nav>
                 <div>
-                    <img src={Logo} alt="Vibrance Logo" />
+                    <img src={Logo} width={160} alt="Vibrance Logo" />
                 </div>
                 <div>
-
+                    <ul className='flex bg-green-500'>
+                        <li>Home</li>
+                        <li>Produtos</li>
+                        <li>BookClub</li>
+                        <li>Ebooks</li>
+                        <li>Contato</li>
+                    </ul>
                 </div>
                 <div>
-
+                    <img src={brazilFlag} width={36} alt="Brazil Flag" />
                 </div>
                 <div>
-
+                    <img src={cartIcon} width={46} alt="Cart" />
+                    <img src={userIcon} width={42} alt="User Profile" />
                 </div>
             </nav>
         </>
