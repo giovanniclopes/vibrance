59356183a3679c087efa28096e43669df8de7f9d
Giovanni Lopes <92595678+giovanniclopes@users.noreply.github.com>
2022-11-16 08:39:24 -0300

    first section updated

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -1,9 +1,22 @@
+import mainBooksImage from '../assets/main-books.png';
+import searchIcon from '../assets/icons/search.png';
+
 export function FirstSection() {
   return (
     <>
-      <div className="bg-firstSection">
-				
-			</div>
+      <section className="bg-firstSection p-44 grid grid-cols-2">
+				<div>
+          <h1>Qual livro/HQ você está procurando?</h1>
+          <p>Não tem certeza do que quer ler a seguir? Explore nosso catálogo de livros.</p>
+          <form action="">
+            <input type="text" placeholder='Pesquisar por um livro...' />
+            <input type="submit" value={searchIcon} />
+          </form>
+        </div>
+        <div>
+          <img src={mainBooksImage} width={630} alt="" />
+        </div>
+			</section>
     </>
   );
 }
