45e45c26b016ec1f1f10443d5c21e5e26cc2ce36
Giovanni Lopes <92595678+giovanniclopes@users.noreply.github.com>
2022-11-16 08:49:19 -0300

    att

--- src/components/FirstSection.tsx
+++ src/components/FirstSection.tsx
@@ -1,20 +1,19 @@
 import mainBooksImage from '../assets/main-books.png';
-import searchIcon from '../assets/icons/search.png';
 
 export function FirstSection() {
   return (
     <>
       <section className="bg-firstSection p-44 grid grid-cols-2">
 				<div>
-          <h1>Qual livro/HQ você está procurando?</h1>
+          <h1 className=''>Qual livro/HQ você está procurando?</h1>
           <p>Não tem certeza do que quer ler a seguir? Explore nosso catálogo de livros.</p>
           <form action="">
             <input type="text" placeholder='Pesquisar por um livro...' />
-            <input type="submit" value={searchIcon} />
+            <input type="submit" className='bg-magenta-900' value=" " />
           </form>
         </div>
         <div>
-          <img src={mainBooksImage} width={630} alt="" />
+          <img src={mainBooksImage} width={850} alt="" />
         </div>
 			</section>
     </>
