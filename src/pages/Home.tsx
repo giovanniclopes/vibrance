1339983928a0929a6279014d08c6930e4f54c5fd
Giovanni Lopes <giovanniclopes@gmail.com>
2023-12-26 23:11:42 -0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import { MagnifyingGlass } from "phosphor-react";
 import HomeDecoration from "../assets/svg/HomeDecoration";
+import { useState } from "react";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchButtonClick = () => {
+    setSearchValue("");
+  };
+
   return (
     <main className="overflow-hidden">
       <div className="grid grid-cols-2 gap-x-5 pt-5 bg-homeFirstSectionImage bg-cover bg-center">
@@ -17,11 +24,17 @@ export default function Home() {
             <input
               className="w-full p-2 py-3 border-2 outline-none font-medium border-product-purple-500 rounded-es-md rounded-ss-md"
               type="search"
-              name="book-search"
-              id="book-search"
+              name="book-input-search"
+              id="book-input-search"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
               placeholder="Pesquise por um livro..."
             />
-            <div className="flex items-center justify-center px-8 bg-product-purple-500 rounded-se-md rounded-ee-md">
+            <div
+              id="search-btn"
+              className="flex items-center justify-center px-8 bg-product-purple-500 rounded-se-md rounded-ee-md cursor-pointer transition-all active:bg-product-purple-400"
+              onClick={handleSearchButtonClick}
+            >
               <MagnifyingGlass size={24} color="#ffffff" />
             </div>
           </div>
