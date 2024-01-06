09ed266fa320765a9dd38cd7353a7b069c8ddca7
Giovanni Lopes <giovanniclopes@gmail.com>
2024-01-06 02:19:19 -0300

    contact page created

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,59 +1,133 @@
 export default function Contact() {
   return (
-    <main className="bg-bgContactPage pt-36">
+    <main className="bg-bgContactPage pt-36 px-12 pb-24">
       <div className="items-start justify-center gap-24">
         <div>
-          <h1 className="font-extrabold text-6xl text-product-purple-500">
+          <h1 className="font-extrabold text-6xl text-product-purple-500 mbl:">
             Perguntas? <br /> Estamos aqui para ajudar.
           </h1>
         </div>
-        <div>
-          <form
-            className="flex flex-col gap-3"
-            action="https://formsubmit.co/giovanniclopes@gmail.com"
-            method="POST"
-          >
-            <div className="flex flex-col items-start justify-center gap-2">
-              <label htmlFor="userName">
-                Nome completo <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="text"
-                name="userName"
-                id="userName"
-                placeholder="Me diga seu nome"
-                required
-              />
+        <div className="grid grid-cols-2 items-start justify-center gap-32 mt-24">
+          <div>
+            <form
+              className="flex flex-col gap-8"
+              action="https://formsubmit.co/giovanniclopes@gmail.com"
+              method="POST"
+            >
+              <div className="flex flex-col items-start justify-center gap-2">
+                <label className="font-medium text-lg" htmlFor="userName">
+                  Nome completo <span className="text-red-500">*</span>
+                </label>
+                <input
+                  className="w-full p-3 rounded border border-gray-300 focus:outline-none focus:border-product-purple-500"
+                  type="text"
+                  name="userName"
+                  id="userName"
+                  placeholder="Me diga seu nome"
+                  required
+                />
+              </div>
+              <div className="flex flex-col items-start justify-center gap-2">
+                <label className="font-medium text-lg" htmlFor="userEmail">
+                  Email <span className="text-red-500">*</span>
+                </label>
+                <input
+                  className="w-full p-3 rounded border border-gray-300 focus:outline-none focus:border-product-purple-500"
+                  type="email"
+                  name="userEmail"
+                  id="userEmail"
+                  placeholder="Me diga seu e-mail"
+                  required
+                />
+              </div>
+              <div className="flex flex-col items-start justify-center gap-2">
+                <label className="font-medium text-lg" htmlFor="userMessage">
+                  Organização
+                </label>
+                <input
+                  className="w-full p-3 rounded border border-gray-300 focus:outline-none focus:border-product-purple-500"
+                  type="text"
+                  name="userOrganization"
+                  id="userOrganization"
+                />
+                <span className="text-sm text-gray-700"> - Insira sua organização, caso possua</span>
+              </div>
+              <div className="flex flex-col items-start justify-center gap-2">
+                <label className="font-medium text-lg" htmlFor="userMessage">
+                  Sua mensagem <span className="text-red-500">*</span>
+                </label>
+                <textarea
+                  className="w-full p-3 rounded border border-gray-300 focus:outline-none focus:border-product-purple-500"
+                  name="userMessage"
+                  id="userMessage"
+                  rows={5}
+                  placeholder="Olá! Eu gostaria de falar sobre {assunto}..."
+                  required
+                />
+              </div>
+              <input type="hidden" name="_template" value="table" />
+              <button
+                aria-label="Enviar mensagem"
+                className="mt-4 px-4 py-5 border-none bg-product-purple-500 text-white font-semibold cursor-pointer rounded transiton-all hover:opacity-90"
+              >
+                Enviar mensagem
+              </button>
+            </form>
+          </div>
+
+          <div className="grid grid-cols-2 gap-20">
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col gap-2">
+                <span className="font-semibold text-lg">Perguntas</span>
+                <span>Confira nosso FAQ:</span>
+                <ul className="text-blue-800 underline">
+                  <li>
+                    <a href="">Livros FAQ</a>
+                  </li>
+                  <li>
+                    <a href="">Equipe FAQ</a>
+                  </li>
+                  <li>
+                    <a href="">BookClub FAQ</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="font-semibold text-lg">Como comprar</span>
+                <span>
+                  Para comprar um produto, você primeiramente precisa ser um
+                  cliente registrado.
+                </span>
+                <ul className="text-blue-800 underline">
+                  <li>
+                    <a href="">Se registre</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="font-semibold text-lg">Onde comprar</span>
+                <span>Nossos produtos estão a venda apenas em nosso site.</span>
+                <ul className="text-blue-800 underline">
+                  <li>
+                    <a href="">Conferir produtos</a>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className="flex flex-col items-start justify-center gap-2">
-              <label htmlFor="userEmail">
-                Email <span className="text-red-500">*</span>
-              </label>
-              <input
-                type="email"
-                name="userEmail"
-                id="userEmail"
-                placeholder="Me diga seu e-mail"
-                required
-              />
+            <div className="flex flex-col gap-10">
+              <div className="flex flex-col gap-2">
+                <span className="font-semibold text-lg">Entre em contato</span>
+                <span>(11) 9 1234-4321</span>
+              </div>
+              <div className="flex flex-col gap-2">
+                <span className="font-semibold text-lg">Vibrance Company©</span>
+                <ul className="flex flex-col gap-2 list-disc">
+                  <li>Rua dos Almirantes, São Caetano, Brasil</li>
+                  <li>Rua Sophia dos Pinhares, Brasília, Brasil</li>
+                </ul>
+              </div>
             </div>
-            <div className="flex flex-col items-start justify-center gap-2">
-              <label htmlFor="userMessage">Organização</label>
-              <input type="text" name="userOrganization" id="userOrganization" />
-            </div>
-            <div className="flex flex-col items-start justify-center gap-2">
-              <label htmlFor="">
-                Sua mensagem <span className="text-red-500">*</span>
-              </label>
-              <textarea
-                name="userMessage"
-                id="userMessage"
-                rows={5}
-                placeholder="Olá! Eu gostaria de falar sobre {assunto}..."
-                required
-              />
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </main>
