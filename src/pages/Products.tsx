16817269c27317a5ebcc94d02a79b265f88968ec
Giovanni Lopes <giovanniclopes@gmail.com>
2024-01-07 21:22:40 -0300

    maintenance warning created

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,7 +1,14 @@
+import MaintenancePage from "../assets/svg/MaintenancePage";
+
 export default function Products() {
   return (
-    <main>
-      Products
+    <main className="mt-44 mbl:mt-32">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <span className="font-bold text-product-purple-500 text-5xl mbl:text-2xl">Página em manutenção</span>
+        <div className="w-1/2 mbl:w-full">
+          <MaintenancePage />
+        </div>
+      </div>
     </main>
   )
 }
